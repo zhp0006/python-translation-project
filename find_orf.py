c18d76f799b4a89623d79b19781f1c502bcc8199
Zahida Hassan Pervaiz <69922404+zhp0006@users.noreply.github.com>
2021-03-21 21:49:24 -0500

    changed codon pattern string regex

--- find_orf.py
+++ find_orf.py
@@ -119,7 +119,7 @@ def vet_codon(codon):
     # Change `codon_pattern_str` so that it will match any valid codons, and
     # only valid codons.
     # Read the docstring above for additional clues.
-    codon_pattern_str = r'[^AUG|AUU$]'
+    codon_pattern_str = r'^[acguACGU]{3}$'
     ##########################################################################
 
     codon_pattern = re.compile(codon_pattern_str)
