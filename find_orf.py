1f1df3a26f8d728248d0b6816eba7a15ef56ec3f
Zahida Hassan Pervaiz <69922404+zhp0006@users.noreply.github.com>
2021-03-21 21:58:04 -0500

    changes the regex for TestFindFirstOrf

--- find_orf.py
+++ find_orf.py
@@ -207,7 +207,7 @@ def find_first_orf(sequence,
     # exactly. Change `orf_pattern_str` so that it will match any open reading
     # frame.
     # Read the docstring above for additional clues.
-    orf_pattern_str = r'AUGGUAUAA'
+    orf_pattern_str = r'(' + '|'.join(start_codons) + ')([AUGC]{3})*(' + '|'.join(stop_codons)+')'
     ##########################################################################
 
     # Create the regular expression object
